5bbf7517be1d9ef1a4a9ab6cfe3f6006de9cafa0
Achu Ajesh <achuv37@gmail.com>
2022-04-28 21:20:04 -0400

    created roles and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,30 @@
 DROP TABLE IF EXISTS departments;
-
-
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
 
 -- Department Table
 CREATE TABLE departments (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(50) NOT NULL
 );
+
+-- Roles Table
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  job_title VARCHAR(50) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INTEGER
+);
+
+-- Employee table
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
